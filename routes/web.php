84487dc6f5f5f54ec7aba106478156706e910643
Hasiru-Herath <hasiruherath.13@gmail.com>
2024-06-16 19:54:23 +0530

    admin_login  customized`

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 Route::middleware(['auth', 'role:agent'])->group(function () {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
-// Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 // Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
