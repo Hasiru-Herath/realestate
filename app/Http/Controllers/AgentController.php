41787c09898bfb6997e27f05ef6d5e931031a8f8
Hasiru-Herath <hasiruherath.13@gmail.com>
2024-06-24 15:00:04 +0530

    agent dashboard

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AgentController extends Controller
 {
     public function AgentDashboard(){
-        return view('agent.agent_dashboard');
+        return view('agent.index');
     }
 }
